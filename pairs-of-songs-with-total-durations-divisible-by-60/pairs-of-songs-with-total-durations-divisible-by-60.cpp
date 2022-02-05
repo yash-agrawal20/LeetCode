7f874a3529c26f7104532fc5fab78fcd4ce2d044
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-02-05 12:25:52 +0530

    Update pairs-of-songs-with-total-durations-divisible-by-60.cpp

--- pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -2,6 +2,19 @@ class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
         
+//         //Brute Force approach
+//         int count = 0;
+        
+//         for(int i = 0; i < time.size() - 1; i++){
+        
+//             for(int j = i + 1; j < time.size(); j++){
+            
+//                 if((time[i] + time[j]) % 60 == 0) count++;
+//             }
+//         }
+        
+//         return count;
+        
         int map[60] = {};
         int result = 0;
         
