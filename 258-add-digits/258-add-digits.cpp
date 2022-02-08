a01127ba447cbb473e2cad1ef96ce496695e7cde
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-02-08 23:14:35 +0530

    Time: 7 ms (11.31%), Space: 5.9 MB (75.31%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        int sum = 0;
+        
+        while(num){
+            
+            sum += num%10;
+            num = num/10;
+        }
+        
+        if(sum <= 9){
+            return sum;
+        }
+        else{
+            return addDigits(sum);
+        }
+    }
+};
