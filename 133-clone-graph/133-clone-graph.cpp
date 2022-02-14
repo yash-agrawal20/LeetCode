45c65430421a37a8f76c3adb523a3ea2272ea600
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-02-14 10:13:56 +0530

    Time: 7 ms (71.75%), Space: 8.9 MB (23.05%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -19,36 +19,53 @@ public:
 };
 */
 
+
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
         
+        //BFS
+//         if(!node)
+//             return NULL;
+        
+//         Node *copy = new Node(node->val, {});
+//         //Unordered Map
+//         copies[node] = copy;
+//         queue<Node*> todo;
+//         todo.push(node);
+        
+//         while(!todo.empty()){
+            
+//             Node* cur = todo.front();
+//             todo.pop();
+            
+//             for(Node* neighbor: cur-> neighbors){
+//                 if(copies.find(neighbor) == copies.end()){
+                    
+//                     copies[neighbor] = new Node(neighbor->val, {});
+//                     todo.push(neighbor);
+//                 }
+                
+//                 copies[cur] -> neighbors.push_back(copies[neighbor]);
+//             }
+//         }
+        
+//         return copy;
+        
+        //DFS
         if(!node)
             return NULL;
         
-        Node *copy = new Node(node->val, {});
-        //Unordered Map
-        copies[node] = copy;
-        queue<Node*> todo;
-        todo.push(node);
-        
-        while(!todo.empty()){
+        if(copies.find(node) == copies.end()){
             
-            Node* cur = todo.front();
-            todo.pop();
-            
-            for(Node* neighbor: cur-> neighbors){
-                if(copies.find(neighbor) == copies.end()){
-                    
-                    copies[neighbor] = new Node(neighbor->val, {});
-                    todo.push(neighbor);
-                }
+            copies[node] = new Node(node-> val, {});
+            for(Node* neighbor: node-> neighbors){
                 
-                copies[cur] -> neighbors.push_back(copies[neighbor]);
+                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
             }
         }
         
-        return copy;
+        return copies[node];
     }
     
 private:
