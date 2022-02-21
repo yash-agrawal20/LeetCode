49deb35acd01ae6fb5b49c6030737e4c4536b95f
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-02-21 13:53:03 +0530

    Time: 26 ms (53.29%), Space: 19.6 MB (52.27%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) { 
+        
+        //Moore's Voting Algorithms
+        int count = 0;
+        int element = 0;
+            
+        for(int num : nums){
+            
+            if(count == 0){
+                element = num;
+            }
+            
+            if(element == num){
+                count += 1;
+            }
+            else{
+                count -= 1;
+            }
+        }
+        
+        return element;
+        
+    }
+};
