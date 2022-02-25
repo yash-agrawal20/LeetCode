5c10cb04214ad72dcee9551792dd43d0a069fd85
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-02-26 01:13:53 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (47.00%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -38,20 +38,38 @@ public:
         // return maxMoney(n-1, nums, dp);
         
         //Tabulation approach
-        vector<int> dp(n, -1);
-        dp[0] = nums[0];
+//         vector<int> dp(n, -1);
+//         dp[0] = nums[0];
+        
+//         for(int i = 1; i < n; i++){
+            
+//             int pick = nums[i];
+//             if(i > 1) pick += dp[i-2];
+            
+//             int notPick = dp[i-1];
+            
+//             dp[i] = max(pick, notPick);
+//         }
+        
+//         return dp[n-1];     
+        
+        //Space Optimization Approach
+        int prev = nums[0];
+        int prev2 = 0;
         
         for(int i = 1; i < n; i++){
             
             int pick = nums[i];
-            if(i > 1) pick += dp[i-2];
+            if(i > 1) pick += prev2;
             
-            int notPick = dp[i-1];
+            int notPick = prev;
             
-            dp[i] = max(pick, notPick);
+            int curr = max(pick, notPick);
+            
+            prev2 = prev;
+            prev = curr;
         }
         
-        return dp[n-1];     
-        
+        return prev;
     }
 };
