0cbe6f74da8ecefe645f6f41e752b041d2c8a8d3
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-02-27 00:34:43 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (41.90%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    int totalPaths(int l, int b){
+        
+        //Base Case
+        if(l == 0 && b == 0) return 1;
+        
+        else if(l < 0 || b < 0) return 0;
+        
+        //Stuff
+        int up = totalPaths(l-1, b);
+        int left = totalPaths(l, b-1);
+        
+        return up + left;
+    }
+    
+    int totalPathsm(int l, int b, vector<vector<int>> &dp){
+        
+        if(l == 0 && b == 0) return 1;
+        
+        else if(l < 0 || b < 0) return 0;
+        
+        else if(dp[l][b] != -1) return dp[l][b];
+        
+        int up = totalPathsm(l-1, b, dp);
+        int left = totalPathsm(l, b-1, dp);
+        
+        return dp[l][b] = up + left;
+    }
+
+public:
+    int uniquePaths(int m, int n) {
+        
+        //Recursion Approach
+        // return totalPaths(m-1, n-1);
+        
+        //Memoization Approach
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return totalPathsm(m-1, n-1, dp);
+        
+    }
+};
