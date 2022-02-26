f6448891e1bee37eea959be6b9419340b46ba368
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-02-27 00:46:36 +0530

    Time: 3 ms (33.67%), Space: 6.6 MB (24.63%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -35,8 +35,33 @@ public:
         // return totalPaths(m-1, n-1);
         
         //Memoization Approach
+        // vector<vector<int>> dp(m, vector<int> (n, -1));
+        // return totalPathsm(m-1, n-1, dp);
+        
+        //Tabulation Approach
         vector<vector<int>> dp(m, vector<int> (n, -1));
-        return totalPathsm(m-1, n-1, dp);
+        
+        for(int i = 0; i < m; i++){
+            
+            for(int j = 0; j < n; j++){
+                
+                if(i == 0 && j == 0) dp[0][0] = 1;
+                
+                else{
+                    
+                    int down = 0, right = 0;
+                    if(i > 0)
+                        down = dp[i-1][j];
+                    
+                    if(j > 0)
+                        right = dp[i][j-1];
+                    
+                    dp[i][j] = down + right;
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
         
     }
 };
