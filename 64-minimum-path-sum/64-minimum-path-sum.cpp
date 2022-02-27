f9671b97156b6ba4007d31e1d547475bb0b5f336
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-02-27 13:30:45 +0530

    Time: 12 ms (61.48%), Space: 10.1 MB (24.18%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -21,10 +21,30 @@ public:
         int n = grid[0].size();
         
         //Memoization Approach
-        vector<vector<int>> dp(m, vector<int> (n, -1));
-        return f(m-1, n-1, grid, dp);
+        // vector<vector<int>> dp(m, vector<int> (n, -1));
+        // return f(m-1, n-1, grid, dp);
         
         //Tabulation Approach
-        // vector<vector<int>> dp(m,  vector<int> (n, -1))
+        vector<vector<int>> dp(m,  vector<int> (n, -1));
+        
+        for(int i = 0; i < m; i++){
+            
+            for(int j = 0; j < n; j++){
+                
+                int down = 20000, right = 20000;
+                if(i > 0){
+                    down = grid[i][j] + dp[i-1][j];
+                }
+                
+                if(j > 0){
+                    right = grid[i][j] + dp[i][j-1]; 
+                }
+                
+                dp[i][j] = min(down, right);
+                if(i == 0 && j == 0) dp[0][0] = grid[0][0];
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
