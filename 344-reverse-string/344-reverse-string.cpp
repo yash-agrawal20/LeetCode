27ab5bccc17b69c40a71d68b9648f961683d070b
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-03-13 14:46:34 +0530

    Time: 26 ms (67.56%), Space: 23.3 MB (42.30%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,18 +1,36 @@
 class Solution {
+    void reversal(vector<char>& s, int start, int end){
+        
+        if(start >= end)
+            return;
+        
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        
+        reversal(s, start+1, end-1);
+    }
+    
 public:
     void reverseString(vector<char>& s) {
         
         //Recursive Approach
         int start = 0;
-        int end = s.size() -1;
+        int end = s.size() - 1;
         
-        while(start < end){
+        reversal(s, start, end);
+        
+        //Iterative Approach
+//         int start = 0;
+//         int end = s.size() -1;
+        
+//         while(start < end){
             
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-            start++;
-            end--;
-        }
+//             char temp = s[start];
+//             s[start] = s[end];
+//             s[end] = temp;
+//             start++;
+//             end--;
+//         }
     }
 };
