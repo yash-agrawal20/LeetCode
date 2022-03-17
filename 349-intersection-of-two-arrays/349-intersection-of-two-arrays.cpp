1b877d6be80d4484132c06e7a8e000e65c70afc7
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-03-17 22:30:14 +0530

    Time: 11 ms (39.19%), Space: 10.4 MB (52.38%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -3,39 +3,25 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
    
 //Using set data structure        
-//         set<int> st;
-//         vector<int> res;
-        
-//         for(int i = 0; i < nums1.size(); i++){
-            
-//             st.insert(nums1[i]);
-//         }
-        
-//         for(int i = 0; i < nums2.size(); i++){
-            
-//             auto it = st.find(nums2[i]);
-            
-//             if(it != st.end()){
-                
-//                 res.push_back(nums2[i]);
-//                 st.erase(it);
-//             }
-//         }
-        
-//         return res;  
-        
-        unordered_set<int> st(nums1.begin(), nums1.end());
+        set<int> st;
         vector<int> res;
         
-        for(auto it: nums2){
+        for(int i = 0; i < nums1.size(); i++){
             
-            if(st.count(it)){
+            st.insert(nums1[i]);
+        }
+        
+        for(int i = 0; i < nums2.size(); i++){
+            
+            auto it = st.find(nums2[i]);
+            
+            if(it != st.end()){
                 
-                res.push_back(it);
+                res.push_back(nums2[i]);
                 st.erase(it);
             }
         }
         
-        return res;
+        return res;        
     }
 };
