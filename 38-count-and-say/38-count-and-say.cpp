0f92578f5aaf4c77dcd4a16037519373a4eef08b
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-04-01 16:14:59 +0530

    Time: 15 ms (39.06%), Space: 7 MB (37.22%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,27 +1,52 @@
 class Solution{
+private:
+    string built(int n, string &res){
+        
+        if(n == 1) return res;
+        
+        string curr = "";
+        for(int i = 0; i < res.size(); i++){
+            
+            int count = 1;
+            while((i+1) < res.size() && res[i] == res[i+1]){
+                i++;
+                count++;
+            }
+            
+            curr += to_string(count) + res[i];
+        }
+        
+        res = curr;
+        
+        return built(n-1, res);
+    }
+
 public:
     string countAndSay(int n) {
         
         string res = "1";
         //--n not n--
-        while(--n){
+        //Iterative Approach
+//         while(--n){
             
-            string curr = "";
-            for(int i = 0; i < res.size(); i++){
+//             string curr = "";
+//             for(int i = 0; i < res.size(); i++){
                 
-                int count  = 1;
-                while((i + 1) < res.size() && (res[i] == res[i+1])){
-                    i++;
-                    count++;
-                }
+//                 int count  = 1;
+//                 while((i + 1) < res.size() && (res[i] == res[i+1])){
+//                     i++;
+//                     count++;
+//                 }
                 
-                curr += to_string(count) + res[i];
-            }
+//                 curr += to_string(count) + res[i];
+//             }
             
-            res = curr;
-        }
+//             res = curr;
+//         }
+        //Recursive Approach
+        return built(n, res);
         
-        return res;        
+        // return res;        
         
     }
 };
