139c0e08898b1916c39ccddd0df345ce5fe34743
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-05-13 20:08:01 +0530

    Time: 38 ms (98.67%), Space: 30 MB (97.98%) - LeetHub

---
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    //TLE
+//     int subarraysDivByK(vector<int>& nums, int k) {
+        
+//         int n = nums.size();
+//         int count = 0;
+        
+//         for(int i = 0; i < n; i++){
+            
+//             int sum = 0;
+//             for(int j =  i; j < n; j++){
+                
+//                 sum += nums[j];
+                
+//                 if(sum % k == 0) count++;
+//             }
+//         }
+        
+//         return count;
+        int subarraysDivByK(vector<int>& A, int K) {
+            
+            vector<int> count(K);
+            count[0] = 1;
+            int prefix = 0, res = 0;
+            for (int a : A) {
+                //IMP
+                prefix = (prefix + a % K + K) % K;
+                res += count[prefix]++;
+            }
+            
+            return res;
+    }
+        
+};
