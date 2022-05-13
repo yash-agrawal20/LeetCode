7ee34169ae11598856769aae27e7a5662b0b1a9c
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-05-13 23:54:58 +0530

    Time: 64 ms (78.94%), Space: 33.4 MB (93.80%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -1,18 +1,35 @@
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/discuss/775798/c%2B%2B-Four-Solution-or-O-(N*N)-to-greater-O(N)-or-Explain-All
+
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         
+//         int n = nums.size();
+//         map<int, int> mp; //Space depends on the size of the vector O(n)
+//         vector<int> res;
+        
+//         for(int i = 0; i < n; i++){
+            
+//             mp[nums[i]]++;
+            
+//             if(mp[nums[i]] == 2){
+//                 res.push_back(nums[i]);
+//             }
+//         }
+        
+//         return res;
+        
+        //Doing the solution in O(1) S.C.
+        
         int n = nums.size();
-        map<int, int> mp;
         vector<int> res;
         
         for(int i = 0; i < n; i++){
             
-            mp[nums[i]]++;
-            
-            if(mp[nums[i]] == 2){
-                res.push_back(nums[i]);
-            }
+            if(nums[abs(nums[i]) - 1] < 0)
+                res.push_back(abs(nums[i]));
+                              
+            nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
         }
         
         return res;
