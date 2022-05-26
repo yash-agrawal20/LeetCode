1253050bb1a13fb1ebc00fdbf700a6d7edda8f2c
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-05-26 10:59:30 +0530

    Time: 6 ms (77.68%), Space: 9 MB (31.03%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,37 +1,67 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+   
+        //TC: O(nm)     
+//         int n = nums1.size();
+//         int m = nums2.size();
         
+//         for(int i = 0; i < n; i++){
+            
+//             for(int j = 0; j < m; j++){
+                
+//                 if(nums1[i] == nums2[j]){
+                    
+//                     int k = j+1;
+                    
+//                     while(k <= m){
+                        
+//                         if(k == m)
+//                             nums1[i] = -1;
+                        
+//                         else if(nums2[k] > nums2[j]){
+//                             nums1[i] = nums2[k];
+//                             break;
+//                         }
+                        
+//                         k++;
+//                     }
+                    
+//                     break;
+//                 }
+//             }
+//         }
+        
+//         return nums1;
+        
+        //TC(n+m)
         int n = nums1.size();
         int m = nums2.size();
         
-        for(int i = 0; i < n; i++){
+        vector<int> res(n, -1);
+        stack<int> s;
+        unordered_map<int, int> mp;
+        
+        //Preprossessing
+        //IMP
+        for(int i = 0; i < m; i++){
             
-            for(int j = 0; j < m; j++){
+            while(s.size() && nums2[i] > s.top()){
                 
-                if(nums1[i] == nums2[j]){
-                    
-                    int k = j+1;
-                    
-                    while(k <= m){
-                        
-                        if(k == m)
-                            nums1[i] = -1;
-                        
-                        else if(nums2[k] > nums2[j]){
-                            nums1[i] = nums2[k];
-                            break;
-                        }
-                        
-                        k++;
-                    }
-                    
-                    break;
-                }
+                mp[s.top()] = nums2[i];
+                s.pop();
             }
+            
+            s.push(nums2[i]);
         }
         
-        return nums1;
+        for(int i = 0; i < n; i++){
+            
+            if(mp.find(nums1[i]) != mp.end())
+                res[i] = mp[nums1[i]];
+        }
+        
+        return res;
         
     }
 };   
