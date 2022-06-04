db3e028ab861192975521f7d1eda8df62617d661
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-06-04 23:52:11 +0530

    Time: 133 ms (16.53%), Space: 15.9 MB (99.65%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -7,26 +7,45 @@ public:
         //Greedy Approach
         //Priority queue solution
         
-        int i = 0;
-        int res;
+//         int i = 0;
+//         int res;
         
-        priority_queue<int> pq;
+//         priority_queue<int> pq;
         
-        for(res = 0; startFuel < target; res++){
+//         for(res = 0; startFuel < target; res++){
             
-            while(i < stations.size() && startFuel >= stations[i][0]){
-                pq.push(stations[i][1]);
-                i++;
+//             while(i < stations.size() && startFuel >= stations[i][0]){
+//                 pq.push(stations[i][1]);
+//                 i++;
+//             }
+            
+//             if(pq.empty()) 
+//                 return -1;
+            
+//             startFuel += pq.top();
+//             pq.pop();
+//         }
+        
+//         return res;
+        
+        //DP solution
+        int n = stations.size();
+        long dp[501] = {startFuel};
+        
+        for(int i = 0; i < n; i++){
+            
+            for(int j = i; j >= 0 && dp[j] >= stations[i][0]; j--){
+                
+                dp[j+1] = max(dp[j+1], dp[j] + stations[i][1]);
             }
-            
-            if(pq.empty()) 
-                return -1;
-            
-            startFuel += pq.top();
-            pq.pop();
         }
         
-        return res;
+        for(int i = 0; i <= n; i++){
+            
+            if(dp[i] >= target)
+                return i;
+        }
         
+        return -1;
     }
 };
