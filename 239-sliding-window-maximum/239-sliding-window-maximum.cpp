a42472b1dfcf2333eba097e5283848aced894b37
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-06-05 20:23:59 +0530

    Time: 637 ms (16.97%), Space: 148.9 MB (21.06%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -2,24 +2,55 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
-        int n = nums.size();
-        deque<int> dq;
-        vector<int> res;
+//         int n = nums.size();
+//         deque<int> dq;
+//         vector<int> res;
         
-        for(int i = 0; i < n; i++){
+//         for(int i = 0; i < n; i++){
             
-            if(dq.size() && dq.front() == (i - k))
-                dq.pop_front();
+//             if(dq.size() && dq.front() == (i - k))
+//                 dq.pop_front();
             
-            while(dq.size() && nums[dq.back()] < nums[i])
-                dq.pop_back();
+//             while(dq.size() && nums[dq.back()] < nums[i])
+//                 dq.pop_back();
             
-            dq.push_back(i);
+//             dq.push_back(i);
             
-            if(i - k + 1 >= 0)
-                res.push_back(nums[dq.front()]);
+//             if(i - k + 1 >= 0)
+//                 res.push_back(nums[dq.front()]);
+//         }
+        
+//         return res;
+        
+        //Arranging the elements in descending order
+        //Element and index
+        priority_queue<pair<int, int>> pq;
+        vector<int> res;
+        int low = 0;
+        int high = k-1;
+        
+        for(int i = 0; i <= high; i++){
+            pq.push({nums[i], i});
         }
         
-        return res;        
+        //Highest element among the first k elements
+        res.push_back(pq.top().first);
+        high++;
+        
+        while(high < nums.size()){
+            
+            //Increasing the lowwer boundary and adding the new element
+            low++;
+            pq.push({nums[high], high});
+            
+            while(pq.size() && pq.top().second < low)
+                pq.pop();
+            
+            res.push_back(pq.top().first);
+            
+            high++;
+        }
+        
+        return res;
     }
 };
