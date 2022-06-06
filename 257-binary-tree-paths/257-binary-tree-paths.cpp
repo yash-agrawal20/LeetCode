a9946366a8d019b3acf3dfff3a369444cec8f1b3
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-06-06 16:40:00 +0530

    Time: 3 ms (85.85%), Space: 12.3 MB (96.33%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -14,6 +14,7 @@ private:
     void f(vector<string>& res, TreeNode* root, string s){
         
         //Base
+        //Leaf Node
         if(!root->left && !root->right){
             
             s += to_string(root->val);
@@ -21,9 +22,11 @@ private:
             return;
         }
         
+        //Stuff
         s += to_string(root->val);
         s += "->";
         
+        //Continue
         if(root->left)
             f(res, root->left, s);
         if(root->right)
@@ -34,12 +37,53 @@ public:
     vector<string> binaryTreePaths(TreeNode* root) {
         
         //Using Recusion
-        vector<string> res;
-        string s;
+        // vector<string> res;
+        // string s;
         
-        f(res, root, s);
+        //f(res, root, s);
+        
+        // return res;
+        
+        //Iterative approach
+        string s = "";
+        vector<string> res;
+        
+        queue<TreeNode*> rtemp;
+        queue<string> stemp;
+        
+        rtemp.push(root);
+        stemp.push(to_string(root->val));
+        
+        while(!rtemp.empty()){
+            
+            TreeNode* temp = rtemp.front();
+            rtemp.pop();
+            
+            string strtemp = stemp.front();
+            stemp.pop();
+            
+            if(!temp->left && !temp->right){
+                
+                res.push_back(strtemp);
+            }
+            
+            else{
+                
+                if(temp->left){
+                    
+                    rtemp.push(temp->left);
+                    stemp.push(strtemp + "->" + to_string(temp->left->val));
+                }
+                
+                if(temp->right){
+                    
+                    rtemp.push(temp->right);
+                    stemp.push(strtemp + "->" + to_string(temp->right->val));
+                }
+            }
+        
+        }
         
         return res;
-        
     }
 };
