f48d599057e487949c550f76a7619dc86614dd70
yash-agrawal20 <77885087+yash-agrawal20@users.noreply.github.com>
2022-06-07 23:51:29 +0530

    Time: 3 ms (96.85%), Space: 13 MB (88.38%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,33 +2,46 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        int n = prices.size();
-        int buy = prices[0];
-        int sell = prices[0];
-        int diff = 0;
-        int total = 0;
+        //Approach 1
+//         int n = prices.size();
+//         int buy = prices[0];
+//         int sell = prices[0];
+//         int diff = 0;
+//         int total = 0;
         
-        for(int i = 1; i < n; i++){
+//         for(int i = 1; i < n; i++){
             
-            diff = prices[i] - prices[i-1];
+//             diff = prices[i] - prices[i-1];
             
-            if(diff < 0){
-                total += sell - buy;
-                buy = prices[i];
-                sell = prices[i];
-            }
+//             if(diff < 0){
+//                 total += sell - buy;
+//                 buy = prices[i];
+//                 sell = prices[i];
+//             }
             
-            else if(diff >= 0){
+//             else if(diff >= 0){
                 
-                sell = prices[i];
-            }
+//                 sell = prices[i];
+//             }
             
-            if(i == (n-1)){
-                total += sell - buy;
-            }
+//             if(i == (n-1)){
+//                 total += sell - buy;
+//             }
+//         }
+        
+//         return total;
+        
+        //Approach 2
+        if(prices.size() == 1)
+            return 0;
+        
+        int profit = 0;
+        for(int i = 1; i < prices.size(); i++){
+            
+            if(prices[i] > prices[i-1])
+                profit += prices[i] - prices[i-1];
         }
         
-        return total;
-        
+        return profit;
     }
 };
